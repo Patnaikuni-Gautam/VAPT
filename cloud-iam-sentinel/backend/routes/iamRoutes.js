6ac2968f51861bc06ef942a9d7d45d2e8d3cbe35
Gautam <patnaikuni.gautam12@gmail.com>
2025-05-16 22:37:40 +0530

    Duplication removal

--- cloud-iam-sentinel/backend/routes/iamRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { analyzeIAMPolicy } = require('../controllers/iamController');
-
-router.post('/submit', analyzeIAMPolicy);
-
-module.exports = router;
